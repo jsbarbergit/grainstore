e3c4771fd8426dece7d104b449604ce93444200a
John Barber <johnb@cloudbuildltd.co.uk>
2020-06-25 19:06:54 +0100

    updated readme and arch diagrams

---
+++ precache-manifest.3ffa01d199280e25c3ce8a62d13f9f92.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6fd5b20cfdc65cfd25b4eaf5f0c428d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2f938b47b7586225e349",
+    "url": "/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "8da0626c681503f846b5",
+    "url": "/static/css/main.4ecf0297.chunk.css"
+  },
+  {
+    "revision": "2f938b47b7586225e349",
+    "url": "/static/js/2.27c78288.chunk.js"
+  },
+  {
+    "revision": "dd2bafddf84d0013ad828f5f5d6dbbaf",
+    "url": "/static/js/2.27c78288.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8da0626c681503f846b5",
+    "url": "/static/js/main.4a46da11.chunk.js"
+  },
+  {
+    "revision": "ae049f03d7ab899b29ce",
+    "url": "/static/js/runtime-main.3686f6e1.js"
+  }
+]);
